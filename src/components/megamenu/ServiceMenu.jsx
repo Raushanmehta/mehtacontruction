38c6f2ef950be128690541f2c66be6523e3f3632
Raushanmehta <raushanmehta2184@gmail.com>
2026-08-10 23:53:54 +0530

    add megamenu

---
+++ src/components/megamenu/ServiceMenu.jsx
@@ -0,0 +1,14 @@
+const ServiceMenu = () => {
+  return (
+    <div className="service-menu">
+      <h3>Our Services</h3>
+      <ul>
+        <li>Service 1</li>
+        <li>Service 2</li>
+        <li>Service 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ServiceMenu;
